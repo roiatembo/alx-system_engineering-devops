f9519d3580ef682a53cd1c2d1bf14ad478841c6c
Roia L Tembo <64688048+roiatembo@users.noreply.github.com>
2024-02-18 22:53:34 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,10 @@
-# fixing wordpress site
-exec { 'fix-wordpress':
-  command => '/path/to/fix-wordpress-script.sh',
+# A puppet manuscript to replace a line in a file on a server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
